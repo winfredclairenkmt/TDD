9a71fa1fc59833c7e52c1c817bc7bf2d4289520c
Nakamatte Winfred Claire <kizzaeverdeen@gmail.com>
2023-10-16 15:26:51 +0300

    test fails if a wrong result is provided

--- test/test.js
+++ test/test.js
@@ -1,11 +1,23 @@
+let chai = require("chai")
+let assert = chai.assert;
+
 class Multiply{
-    constructor(a,b){
-        this.a = a;
-        this.b = b;
+    constructor(){
     }
-    multiply(){
-        return this.a * this.b;
+    multiply(a,b){
+        let result = a*b
+        return result
     }
 } 
+
+let multiplier = new Multiply;
+
+describe("test multiplication", function(){
+    it("should multiply two numbers", function(){
+
+        let result = multiplier.multiply(2,5)
+        assert.equal(result ,4);
+    })
+})
 
 // example in use
